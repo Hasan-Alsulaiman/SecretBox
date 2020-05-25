8ea3a714c6f8e5ce7e092eba15faec379b302685
Hasan-Ali <careerforma@gmail.com>
2020-05-25 19:24:02 +0300

    qr

---
+++ qr.py
@@ -0,0 +1,21 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+from pyzbar.pyzbar import decode
+
+# takes payload and name and returns qr code of that payload using that name
+
+
+def gen(payload, name,path):
+    if(type(payload)!= type("string")):
+        payload = str(payload)
+    # Generate QR code
+    try:
+        url = pyqrcode.create(payload)
+        # Create and save the png file naming "myqr.png"
+        finalpath = path + name+'.png'
+        url.png(file = finalpath, scale=6)
+    except:
+        print("failed to generate qr image for", name)
+        pass
+
--- user1.py
+++ user1.py
@@ -4,54 +4,62 @@ import pickle
 import keygen
 import aes
 import keysharing
+import qr
 
 name = "user1"
 peername = "user2"
 d = input("do you want to generate new private/public keys?<y>/<n>\n")
 
 if (d == 'y'):
+    print("generating new key pair...")
     keygen.gen(name)
-else:
-    o = input("pick operation\n [1]encrypt\n [2]decrypt\n")
 
-    # for encryption
-    if(o == '1'):
-        peerpublickeypath = "./peer_publickey/"+peername+"PublicKey.pem"
-        peerpublickeyexits = os.path.isfile(peerpublickeypath)
-        if(peerpublickeyexits):
-            p = input("pick a password: ")
-            msg = input("enter a message: ")
-            encmsg, iv = aes.enc(msg, p)
-            print(encmsg)
-            # key sharing
-            print("peer's public key was found, exchanging password..")
-            encryptedpassword = keysharing.enc(peerpublickeypath, p)
-            print(encryptedpassword)
-            with open(name+"PublicKey.pem", "r") as f:
-                mypubkey = f.read()
-            keyinfo = {"password": encryptedpassword, "iv": iv,
-                       name+"publickey": mypubkey, "encmsg": encmsg}
-            # we save the encrypted msg and key info to file
-            with open("./outbox/"+name+'_msg.txt', 'wb+')as f:
-                f.write(pickle.dumps(keyinfo))
-            print("msg encrypted and saved.")
-        else:
-            print("can find peer's public key file, please add it to directory")
-    if(o == '2'):
-        # change name to peername for final application
-        encryptedmsgpath = "./inbox/"+peername+"_msg.txt"
-        encryptedmsgexits = os.path.isfile(encryptedmsgpath)
-        if(encryptedmsgexits):
-            # load encrypted msg
-            with open("./inbox/"+peername+'_msg.txt', 'rb+')as f:
-                encryptedmsg = pickle.loads(f.read())
-            print("msg found.")
-            print(encryptedmsg)
-            password_dec = encryptedmsg['password']
-            iv_dec = encryptedmsg['iv']
-            password_dec = keysharing.dec(name+"PrivateKey.pem",b"hi",encryptedmsg['password'])
-            iv_dec = encryptedmsg['iv']
-            decryptedmsg = aes.dec(encryptedmsg['encmsg'],password_dec,iv_dec)
-            print(decryptedmsg)            
-        else:
-            print("cant find the msg")
+
+o = input("pick operation\n [1]encrypt\n [2]decrypt\n")
+
+# for encryption
+if(o == '1'):
+    peerpublickeypath = "./peer_publickey/"+peername+"PublicKey.pem"
+    peerpublickeyexits = os.path.isfile(peerpublickeypath)
+    if(peerpublickeyexits):
+        p = input("pick a password: ")
+        msg = input("enter a message: ")
+        encmsg, iv = aes.enc(msg, p)
+        print(encmsg)
+        # key sharing
+        print("peer's public key was found, encrypting the password with his public key..")
+        encryptedpassword = keysharing.enc(peerpublickeypath, p)
+        print(encryptedpassword)
+        with open(name+"PublicKey.pem", "r") as f:
+            mypubkey = f.read()
+        keyinfo = {"password": encryptedpassword, "iv": iv,
+                    name+"publickey": mypubkey, "encmsg": encmsg}
+        # we save the encrypted msg and key info to file
+        with open("./outbox/"+name+'_msg.txt', 'wb+')as f:
+            f.write(pickle.dumps(keyinfo))
+        qrimage = input("would you like to generate qr images? <y>/<n>")
+        if(qrimage == 'y'):
+            qr.gen([{"password":encryptedpassword,"iv":iv}],"password-info",'./outbox/')
+            qr.gen(mypubkey,name+"publickey","./outbox/")
+            qr.gen(encmsg,"encmsg",'./outbox/')
+        print("msg encrypted and saved.")
+    else:
+        print("can find peer's public key file, please add it to directory")
+# for decryption
+if(o == '2'):
+    # change name to peername for final application
+    encryptedmsgpath = "./inbox/"+peername+"_msg.txt"
+    encryptedmsgexits = os.path.isfile(encryptedmsgpath)
+    if(encryptedmsgexits):
+        # load encrypted msg
+        with open("./inbox/"+peername+'_msg.txt', 'rb+')as f:
+            encryptedmsg = pickle.loads(f.read())
+        print("msg found.")
+        print(encryptedmsg)
+        password_dec = keysharing.dec(name+"PrivateKey.pem",b"hi",encryptedmsg['password'])
+        iv_dec = encryptedmsg['iv']
+        decryptedmsg = aes.dec(encryptedmsg['encmsg'],password_dec,iv_dec)
+        print(decryptedmsg)
+        
+    else:
+        print("cant find it")
